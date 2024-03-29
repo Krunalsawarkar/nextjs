18b56c7df9938eddebb9caf46177bc6473b0b745
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2024-03-29 08:49:42 +0530

    learning commit

---
+++ my-app/src/app/login/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <div>
+      Login Page
+      <Link href="/">Go to Home</Link>
+    </div>
+  );
+};
+
+export default page;
--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,15 +1,18 @@
 "use client";
+import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const takeFruit=(item)=>{
-    alert("Take Fruit"+' '+item)
-  }
+  const router = useRouter();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Home Page</h1>
-      <button onClick={() => alert("Testing Click")}>Click Here</button>
-      <button onClick={()=>takeFruit('Orange')}>Fruit Click</button>
+      <Link href="/login">Login Here</Link>
+      <Link href="/about">About Here</Link>
+
+      <button onClick={() => router.push("/login")}>Login Button</button>
+      <button onClick={() => router.push("/about")}>About Button</button>
     </main>
   );
 }
